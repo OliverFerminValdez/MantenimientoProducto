8a3b34cdf49950ea7b5a0c22e36d8f1b05f15d37
Oliver Jose Fermin Valdez <oliver_fermin@ucne.edu.do>
2021-03-14 10:45:42 -0400

    Diseñando ventanas

--- MantenimientoProducto/Form1.Designer.cs
+++ MantenimientoProducto/Form1.Designer.cs
@@ -28,57 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.productoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.registroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.consultaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
+            this.ListarButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.BuscarBtn = new System.Windows.Forms.Button();
+            this.EliminarBtn = new System.Windows.Forms.Button();
+            this.GuardarButton = new System.Windows.Forms.Button();
+            this.NuevoBtn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.CategoriaComboBox = new System.Windows.Forms.ComboBox();
+            this.CostoNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.CodigoTb = new System.Windows.Forms.TextBox();
+            this.DescripcionTb = new System.Windows.Forms.TextBox();
+            this.IDNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CostoNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IDNumericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // ListarButton
             // 
-            this.menuStrip1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.productoToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(860, 28);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.ListarButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ListarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListarButton.Location = new System.Drawing.Point(584, 31);
+            this.ListarButton.Name = "ListarButton";
+            this.ListarButton.Size = new System.Drawing.Size(173, 35);
+            this.ListarButton.TabIndex = 53;
+            this.ListarButton.Text = "Listar Productos";
+            this.ListarButton.UseVisualStyleBackColor = false;
             // 
-            // productoToolStripMenuItem
+            // button2
             // 
-            this.productoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.registroToolStripMenuItem,
-            this.consultaToolStripMenuItem});
-            this.productoToolStripMenuItem.Name = "productoToolStripMenuItem";
-            this.productoToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
-            this.productoToolStripMenuItem.Text = "Producto";
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(426, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 35);
+            this.button2.TabIndex = 52;
+            this.button2.Text = "Agregar";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // registroToolStripMenuItem
+            // BuscarBtn
             // 
-            this.registroToolStripMenuItem.Name = "registroToolStripMenuItem";
-            this.registroToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.registroToolStripMenuItem.Text = "Registro";
+            this.BuscarBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BuscarBtn.Location = new System.Drawing.Point(367, 26);
+            this.BuscarBtn.Name = "BuscarBtn";
+            this.BuscarBtn.Size = new System.Drawing.Size(78, 44);
+            this.BuscarBtn.TabIndex = 51;
+            this.BuscarBtn.Text = "Buscar";
+            this.BuscarBtn.UseVisualStyleBackColor = true;
             // 
-            // consultaToolStripMenuItem
+            // EliminarBtn
             // 
-            this.consultaToolStripMenuItem.Name = "consultaToolStripMenuItem";
-            this.consultaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.consultaToolStripMenuItem.Text = "Consulta";
+            this.EliminarBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EliminarBtn.Location = new System.Drawing.Point(409, 395);
+            this.EliminarBtn.Name = "EliminarBtn";
+            this.EliminarBtn.Size = new System.Drawing.Size(106, 56);
+            this.EliminarBtn.TabIndex = 50;
+            this.EliminarBtn.Text = "Eliminar";
+            this.EliminarBtn.UseVisualStyleBackColor = true;
+            // 
+            // GuardarButton
+            // 
+            this.GuardarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GuardarButton.Location = new System.Drawing.Point(255, 395);
+            this.GuardarButton.Name = "GuardarButton";
+            this.GuardarButton.Size = new System.Drawing.Size(106, 56);
+            this.GuardarButton.TabIndex = 49;
+            this.GuardarButton.Text = "Guardar";
+            this.GuardarButton.UseVisualStyleBackColor = true;
+            // 
+            // NuevoBtn
+            // 
+            this.NuevoBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NuevoBtn.Location = new System.Drawing.Point(108, 395);
+            this.NuevoBtn.Name = "NuevoBtn";
+            this.NuevoBtn.Size = new System.Drawing.Size(106, 56);
+            this.NuevoBtn.TabIndex = 48;
+            this.NuevoBtn.Text = "Nuevo";
+            this.NuevoBtn.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(104, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 24);
+            this.label6.TabIndex = 47;
+            this.label6.Text = "Descripcion";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(104, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 24);
+            this.label5.TabIndex = 46;
+            this.label5.Text = "Categoria";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(104, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 24);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "Costo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 24);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Precio";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(104, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 24);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Codigo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(104, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 24);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "ID";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown3.Location = new System.Drawing.Point(241, 285);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(179, 28);
+            this.numericUpDown3.TabIndex = 41;
+            // 
+            // CategoriaComboBox
+            // 
+            this.CategoriaComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CategoriaComboBox.FormattingEnabled = true;
+            this.CategoriaComboBox.Location = new System.Drawing.Point(241, 185);
+            this.CategoriaComboBox.Name = "CategoriaComboBox";
+            this.CategoriaComboBox.Size = new System.Drawing.Size(179, 30);
+            this.CategoriaComboBox.TabIndex = 40;
+            // 
+            // CostoNumericUpDown
+            // 
+            this.CostoNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CostoNumericUpDown.Location = new System.Drawing.Point(241, 251);
+            this.CostoNumericUpDown.Name = "CostoNumericUpDown";
+            this.CostoNumericUpDown.Size = new System.Drawing.Size(179, 28);
+            this.CostoNumericUpDown.TabIndex = 39;
+            // 
+            // CodigoTb
+            // 
+            this.CodigoTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CodigoTb.Location = new System.Drawing.Point(241, 81);
+            this.CodigoTb.Name = "CodigoTb";
+            this.CodigoTb.Size = new System.Drawing.Size(179, 28);
+            this.CodigoTb.TabIndex = 38;
+            // 
+            // DescripcionTb
+            // 
+            this.DescripcionTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DescripcionTb.Location = new System.Drawing.Point(241, 151);
+            this.DescripcionTb.Name = "DescripcionTb";
+            this.DescripcionTb.Size = new System.Drawing.Size(179, 28);
+            this.DescripcionTb.TabIndex = 37;
+            // 
+            // IDNumericUpDown
+            // 
+            this.IDNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDNumericUpDown.Location = new System.Drawing.Point(241, 35);
+            this.IDNumericUpDown.Name = "IDNumericUpDown";
+            this.IDNumericUpDown.Size = new System.Drawing.Size(120, 28);
+            this.IDNumericUpDown.TabIndex = 36;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(860, 477);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.ClientSize = new System.Drawing.Size(796, 477);
+            this.Controls.Add(this.ListarButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.BuscarBtn);
+            this.Controls.Add(this.EliminarBtn);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.NuevoBtn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.CategoriaComboBox);
+            this.Controls.Add(this.CostoNumericUpDown);
+            this.Controls.Add(this.CodigoTb);
+            this.Controls.Add(this.DescripcionTb);
+            this.Controls.Add(this.IDNumericUpDown);
             this.Name = "Form1";
             this.Text = "Mantenimiento de producto";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CostoNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IDNumericUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,10 +256,24 @@
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem productoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem registroToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem consultaToolStripMenuItem;
+        private System.Windows.Forms.Button ListarButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button BuscarBtn;
+        private System.Windows.Forms.Button EliminarBtn;
+        private System.Windows.Forms.Button GuardarButton;
+        private System.Windows.Forms.Button NuevoBtn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.ComboBox CategoriaComboBox;
+        private System.Windows.Forms.NumericUpDown CostoNumericUpDown;
+        private System.Windows.Forms.TextBox CodigoTb;
+        private System.Windows.Forms.TextBox DescripcionTb;
+        private System.Windows.Forms.NumericUpDown IDNumericUpDown;
     }
 }
 
---
+++ MantenimientoProducto/GUI/Consultas/C_Productos.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace MantenimientoProducto.GUI.Consultas
+{
+    partial class C_Productos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.CriterioComboBox = new System.Windows.Forms.TextBox();
+            this.ConsultarBtn = new System.Windows.Forms.Button();
+            this.Filtro = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 118);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(841, 389);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(304, 30);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // CriterioComboBox
+            // 
+            this.CriterioComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CriterioComboBox.Location = new System.Drawing.Point(323, 82);
+            this.CriterioComboBox.Name = "CriterioComboBox";
+            this.CriterioComboBox.Size = new System.Drawing.Size(364, 28);
+            this.CriterioComboBox.TabIndex = 2;
+            // 
+            // ConsultarBtn
+            // 
+            this.ConsultarBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConsultarBtn.Location = new System.Drawing.Point(693, 80);
+            this.ConsultarBtn.Name = "ConsultarBtn";
+            this.ConsultarBtn.Size = new System.Drawing.Size(161, 30);
+            this.ConsultarBtn.TabIndex = 3;
+            this.ConsultarBtn.Text = "Consultar";
+            this.ConsultarBtn.UseVisualStyleBackColor = true;
+            // 
+            // Filtro
+            // 
+            this.Filtro.AutoSize = true;
+            this.Filtro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Filtro.Location = new System.Drawing.Point(12, 55);
+            this.Filtro.Name = "Filtro";
+            this.Filtro.Size = new System.Drawing.Size(51, 24);
+            this.Filtro.TabIndex = 4;
+            this.Filtro.Text = "Filtro";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(319, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Criterio";
+            // 
+            // C_Productos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 519);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Filtro);
+            this.Controls.Add(this.ConsultarBtn);
+            this.Controls.Add(this.CriterioComboBox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "C_Productos";
+            this.Text = "C_Productos";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox CriterioComboBox;
+        private System.Windows.Forms.Button ConsultarBtn;
+        private System.Windows.Forms.Label Filtro;
+        private System.Windows.Forms.Label label1;
+    }
+}
